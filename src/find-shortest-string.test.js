3f6d30bf4af9c5a63ed1f9b25d21e6d7789a760a
Hanna <sophianhanna@gmail.com>
2021-10-14 21:59:12 +0100

    Fixed formatting and added test file to eslintignore

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -5,10 +5,10 @@
 const findShortestString = require("./find-shortest-string");
 
 test("findShortestString returns the shortest string in an array", () => {
-    expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
-    expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+  expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
 });
 
 test("findShortestString returns the first string if the shortest strings are the same length", () => {
-    expect(findShortestString(["brave", "dance"])).toBe("brave");
+  expect(findShortestString(["brave", "dance"])).toBe("brave");
 });
